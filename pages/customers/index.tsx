6513144f9b097e9d77e7e64da79a38f5c4d75e12
DithyaAnil <adithyaanil002@gmail.com>
2024-09-24 19:42:50 +0530

    Static site generation

--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,7 +1,48 @@
-import { NextPage } from "next";
+import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 
-const Customers: NextPage = () => {
-  return <h1>Customers</h1>;
+type Customer = {
+  id: number;
+  name: string;
+  industry: string;
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {
+      customers: [
+        {
+          id: 1,
+          name: "Eva Martin",
+          industry: "Textiles",
+        },
+        {
+          id: 2,
+          name: "John victor",
+          industry: "Business",
+        },
+      ] as Customer[],
+    },
+  };
+};
+
+const Customers: NextPage = ({
+  customers,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(customers);
+  return (
+    <>
+      <h1>Customers</h1>;
+      {customers.map((customer: Customer) => {
+        return (
+          <div>
+            <p>{customer.name}</p>
+            <p>{customer.id}</p>
+            <p>{customer.industry}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Customers;
