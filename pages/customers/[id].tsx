68d185f930d396dd70a1ee105f649a4587aea17f
DithyaAnil <adithyaanil002@gmail.com>
2024-09-26 22:49:48 +0530

    Details API by Object ID

--- pages/customers/[id].tsx
+++ pages/customers/[id].tsx
@@ -1,11 +1,14 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import { Customer } from "./index";
-import axios, { AxiosError } from "axios";
+//Error : Import 'Customer' conflicts with local value, so must be declared with a type-only import when 'isolatedModules' is enabled.
+//Renamed last default export to CustomerPage to fix this
+//import axios, { AxiosError } from "axios";
 import { ParsedUrlQuery } from "querystring";
-import { ObjectId } from "mongodb";
 import clientPromise from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
 import { BSONError } from "bson";
+import { getCustomer } from "../api/customers/[id]";
 
 type Props = {
   customer?: Customer;
@@ -16,12 +19,12 @@ interface Params extends ParsedUrlQuery {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  /*const result = await axios.get("http://localhost:8000/api/customers/");
-  const paths = result.data.customers.map((customer: Customer) => {
-    return {
-      params: { id: customer.id.toString() },
-    };
-  });*/
+  //   const result = await axios.get("http://localhost:8000/api/customers/");
+  //   const paths = result.data.customers.map((customer: Customer) => {
+  //     return {
+  //       params: { id: customer.id.toString() },
+  //     };
+  //   });
 
   return {
     paths: [],
@@ -33,13 +36,9 @@ export const getStaticProps: GetStaticProps<Props, Params> = async (
   context
 ) => {
   const params = context.params!;
-  try {
-    const mongoClient = await clientPromise;
 
-    const data = (await mongoClient
-      .db()
-      .collection("customers")
-      .findOne({ _id: new ObjectId(params.id) })) as Customer;
+  try {
+    const data = await getCustomer(params.id);
 
     console.log("!!!", data);
 
@@ -57,22 +56,23 @@ export const getStaticProps: GetStaticProps<Props, Params> = async (
       revalidate: 60,
     };
   } catch (err) {
-    console.error(err);
+    console.log(err);
     if (BSONError) {
       return {
         notFound: true,
       };
     }
+    throw err;
   }
-  throw err;
 };
 
 const CustomerPage: NextPage<Props> = (props) => {
   const router = useRouter();
   if (router.isFallback) {
-    return <p>Loading...</p>;
+    return <p>Loading..</p>;
   }
-  return <h1>{props.customer ? Customer : null}</h1>;
+
+  return <h1>{props.customer ? "Customer " + props.customer.name : null}</h1>;
 };
 
 export default CustomerPage;
--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,6 +1,6 @@
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
-import axios from "axios";
-import { MongoClient, ObjectId } from "mongodb";
+//import axios from "axios";
+import { ObjectId } from "mongodb";
 import clientPromise from "../../lib/mongodb";
 import { getCustomers } from "../api/customers";
 
@@ -12,12 +12,13 @@ export type Customer = {
 
 export const getStaticProps: GetStaticProps = async () => {
   const data = await getCustomers();
+
   console.log("!!!", data);
 
-  /*const result = await axios.get<{
-    customers: Customer[];
-  }>("http://localhost:8000/api/customers/");
-  console.log(result.data.customers);*/
+  //   const result = await axios.get<{
+  //     customers: Customer[];
+  //   }>("http://localhost:8000/api/customers/");
+  //   console.log(result.data.customers);
 
   return {
     props: {
@@ -30,16 +31,15 @@ export const getStaticProps: GetStaticProps = async () => {
 const Customers: NextPage = ({
   customers,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  console.log(customers);
   return (
     <>
       <h1>Customers</h1>
       {customers.map((customer: Customer) => {
         return (
           <div key={customer._id.toString()}>
-            <p>{customer._id.toString()}</p>
             <p>{customer.name}</p>
             <p>{customer.industry}</p>
+            <p>{customer._id.toString()}</p>
           </div>
         );
       })}
